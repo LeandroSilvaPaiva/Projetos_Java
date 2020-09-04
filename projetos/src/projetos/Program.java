b583b0ec36f59ec1ec76e65414f1e37fdf81d455
unknown <leandroefernandapaiva@gmail.com>
2020-09-04 17:24:23 -0300

    exercícios

---
+++ projetos/src/projetos/Program.java
@@ -0,0 +1,14 @@
+package projetos;
+
+import java.util.Scanner;
+
+public class Program 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+
+}
+
---
+++ projetos/src/projetos/Programagit.java
@@ -0,0 +1,14 @@
+package projetos;
+
+import java.util.Scanner;
+
+public class Programagit
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+
+}
+
